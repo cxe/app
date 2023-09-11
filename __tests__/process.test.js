7920f19402458007cbe10a44e7f3babdbeec4c18
icarter <ian.carter@holidaycheck.com>
2023-09-11 11:03:25 +0200

    0.3.1 process.run

---
+++ __tests__/process.test.js
@@ -0,0 +1,11 @@
+import { run } from '../process.js';
+
+describe('run', () => {
+    it('should return output of valid command', () => {
+        expect(run('pwd')).toBe(import.meta.url.slice(7, -26));
+    });
+
+    it('should fail for invalid commands', () => {
+        expect(() => run('foobarbazqux')).toThrow();
+    });
+});
